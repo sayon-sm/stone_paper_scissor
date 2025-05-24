80a9e9b996bd8adf3139764bc12a591835a060c3
sm <its.sayon.mitra@gmail.com>
2025-05-24 23:04:45 +0530

    fixing logical error, adding global constants

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+// global constants to omit typos
+const STONE = 'STONE';
+const PAPER = 'PAPER';
+const SCISSOR = 'SCISSOR';
+
 const startGameBtn = document.getElementById('start-game-btn');
 let stat = true;
 
@@ -8,25 +13,26 @@ function start() {
 }
 
 function compChoice(n) {
-  if (n < 3) return 'SCISSOR';
-  else if (n > 6) return 'PAPER';
-  else return 'STONE';
+  if (n < 0.3) return SCISSOR;
+  else if (n > 0.6) return PAPER;
+  else return STONE;
 }
 
 function game() {
   const player = prompt('Enter your choice! \nSTONE \nPAPER \nSCISSOR');
   const comp = compChoice(Math.random());
+  console.log(player, comp);
 
   if (
-    (player === 'STONE' && comp === 'SCISSOR') ||
-    (player === 'PAPER' && comp === 'STONE') ||
-    (player === 'SCISSOR' && comp === 'PAPER')
+    (player === STONE && comp === SCISSOR) ||
+    (player === PAPER && comp === STONE) ||
+    (player === SCISSOR && comp === PAPER)
   )
     alert('PLAYER WON !');
   else if (
-    (player === 'STONE' && comp === 'PAPER') ||
-    (player === 'PAPER' && comp === 'SCISSOR') ||
-    (player === 'SCISSOR' && comp === 'STONE')
+    (player === STONE && comp === PAPER) ||
+    (player === PAPER && comp === SCISSOR) ||
+    (player === SCISSOR && comp === STONE)
   )
     alert('MOSNTER WON ! \nbetter luck next time');
   else alert("THAT'S A DRAW !");
