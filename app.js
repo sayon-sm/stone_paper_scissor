750b66f1e979f55b3246200bde304264b2e11dc8
sm <its.sayon.mitra@gmail.com>
2025-05-24 22:04:49 +0530

    adding button functionality

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 const startGameBtn = document.getElementById('start-game-btn');
+game = true;
+
+function start() {
+  game ? console.log('GAME IS STARTING ...') : alert('GAME IS RUNNING !');
+  game = false;
+}
+
+startGameBtn.addEventListener('click', start);
